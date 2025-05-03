ccf2b2996ef6061e983eca6defe2d77c0449a516
AHMED <ahmed.barq03@gmail.com>
2025-05-03 12:41:40 +0200

    feat: add output caching to currency retrieval and evict cache on update

--- Currency.Api/Currency.Api/Endpoints/CurrencyEndpoint.cs
+++ Currency.Api/Currency.Api/Endpoints/CurrencyEndpoint.cs
@@ -3,6 +3,7 @@ using Currencey.Api.Repository;
 using Currencey.Contact;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
 
 namespace Currencey.Api.Endpoints;
 
@@ -12,15 +13,17 @@ public static class CurrencyEndpoint
     public static void MapCurrency(this IEndpointRouteBuilder app)
     {
         app.MapPut(ApiRoute.Currency.Update, Update);
+        
         app.MapGet(ApiRoute.Currency.GetAll, GetAll)
             .CacheOutput("currencyCache")
             .WithTags("currency");
     }
     
-    static async Task<Results<Ok<decimal>,BadRequest>> Update([FromRoute]string id, decimal amount,[FromServices]ICurrencyRepository currencyRepository, CancellationToken cancellationToken = default)
+    static async Task<Results<Ok<decimal>,BadRequest>> Update([FromRoute]string id, decimal amount,[FromServices]ICurrencyRepository currencyRepository,IOutputCacheStore cacheStore, CancellationToken cancellationToken = default)
     {
         if(await currencyRepository.UpdateCurrencyAsync(id, amount, cancellationToken))
         {
+            await cacheStore.EvictByTagAsync("currency",cancellationToken);
             return TypedResults.Ok(amount);
         }
         return TypedResults.BadRequest();
